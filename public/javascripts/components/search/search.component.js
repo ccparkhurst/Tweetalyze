e9a9ee2b35987d627021bb6a45f282c8313ce330
Christina Parkhurst <ccparkhurst@users.noreply.github.com>
2017-02-15 10:44:24 -0500

    removed twitter rest api

--- public/javascripts/components/search/search.component.js
+++ public/javascripts/components/search/search.component.js
@@ -1,13 +1,6 @@
 app.component('search', {
     templateUrl: '/javascripts/components/search/search.html',
     controller: function(twitterService, $state) {
-
-        this.searchQuery = null;
-
-        this.search = function() {
-            console.log(this.searchQuery)
-            twitterService.getSearchResults(this.searchQuery);
-        };
         
         this.streamQuery = null;
 
